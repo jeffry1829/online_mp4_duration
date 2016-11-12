833f0a9877e7b37a6848e69fc94ad596a0c75096
petjelinux <jeffry1829@gmail.com>
2016-11-12 17:19:40 +0800

    broken

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-var request = require('request');
-var range = new require('http-range').Range('bytes', '36-44'); // i need to check the range...
-
+var http = require('http');
+var urlp = require('url').parse;
+// 36-44
 function run(url, cb){
-    var options = {
-        url: url,
-        headers: {
-            'Content-type': 'application/octet-stream',
-            'Range': range
-        }
-    };
-    request(options, function(err, res, body){
-        if(err){
-            cb(err);
-            return;
-        }
+    var req = http.request(url, function(res){
+        console.log('inininin');
+        res.on('readable', function(){
+            var chunk = readable.read(44);
+        });
+        req.end();
         console.log(res.headers);
         try{
             var buf = new Buffer(body, 'binary');
-            var time_scale = buf.slice(0,4).readInt32LE();
-                console.log('time_scale => '+time_scale);
-            var duration = buf.slice(4,8).readInt32LE();
-                console.log('duration => '+duration);
+            var time_scale = buf.slice(36,40).readInt32BE();
+            console.log('time_scale => '+time_scale);
+            var duration = buf.slice(40,44).readInt32BE();
+            console.log('duration => '+duration);
             var result = duration/time_scale;
             cb(null, result);
         }catch(err){
             cb(err);
         }
     });
+    req.on('error', function(e){
+        console.log('problem with request: '+e.message);
+    });
+
 }
 module.exports = run;
