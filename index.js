fbaf6bec985989280c2e5bce049765c284082739
petjelinux <jeffry1829@gmail.com>
2016-11-12 18:39:11 +0800

    broken :(

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
 var http = require('http');
-var urlp = require('url').parse;
-// 36-44
 function run(url, cb){
     var req = http.request(url, function(res){
-        console.log('inininin');
         res.on('readable', function(){
-            var chunk = readable.read(44);
+            var chunk = res.read(52);
+            if(chunk === null) return;
+            console.log(chunk);
+            req.abort();
+            try{
+                var buf = new Buffer(chunk);
+                var time_scale = buf.slice(44,48).readInt32BE();
+                var duration = buf.slice(48,52).readInt32BE();
+                var result = duration/time_scale;
+                cb(null, result);
+            }catch(err){
+                cb(err);
+            }
         });
-        req.end();
-        console.log(res.headers);
-        try{
-            var buf = new Buffer(body, 'binary');
-            var time_scale = buf.slice(36,40).readInt32BE();
-            console.log('time_scale => '+time_scale);
-            var duration = buf.slice(40,44).readInt32BE();
-            console.log('duration => '+duration);
-            var result = duration/time_scale;
-            cb(null, result);
-        }catch(err){
-            cb(err);
-        }
     });
+    req.end();
     req.on('error', function(e){
         console.log('problem with request: '+e.message);
     });
-
 }
 module.exports = run;
--- test.js
+++ test.js
@@ -1,8 +1,7 @@
 var omd = require('./index.js');
-omd('http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4', function(err,duration){
+omd('http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_2mb.mp4', function(err,duration){
     if(!err){
         console.log(duration);
-        console.log('should be 5sec');
     }else{
         console.log(err);
     }
