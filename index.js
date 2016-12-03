6c1fec0745de8513033b88de07ced86722714dbd
petjelinux <jeffry1829@gmail.com>
2016-12-03 23:03:34 +0800

    add full_scan and test :D

--- index.js
+++ index.js
@@ -3,49 +3,10 @@ var https_anc = require('follow-redirects').https;
 var urlp = require('url').parse;
 var Range = require('http-range').Range;
 var range = new Range('bytes', '-10240');
-function run(url, cb){
-    var http;
-    var protocol = urlp(url).protocol;
-    if(protocol === 'https:'){
-        http = https_anc;
-    }else if(protocol === 'http:'){
-        http = http_anc;
-    }else{
-        cb('it seems that the url is not http nor https');
-    }
-    var req = http.request(url, function(res){
-        var count = 0;
-        res.on('readable', function(){
-            var chunk = res.read();
-            var offset;
-            if(chunk === null) return;
-            count+=chunk.length;
-            if((offset = chunk.indexOf(new Buffer([0x6D, 0x76, 0x68, 0x64]))) !== -1){
-                req.abort();
-                try{
-                    var buf = new Buffer(chunk);
-                    var time_scale = buf.slice(offset+16,offset+20).readInt32BE();
-                    var duration = buf.slice(offset+20,offset+24).readInt32BE();
-                    var result = duration/time_scale;
-                    cb(null, result);
-                }catch(err){
-                    cb(err);
-                }
-            }else{
-                if(count>10240){
-                    req_tail(http, url, cb);
-                    req.abort();
-                    return;
-                }
-            }
-        });
-    });
-    req.end();
-    req.on('error', function(e){
-        console.log('problem with request: '+e.message);
-    });
-}
-function req_tail(http, url, cb){
+function run(obj, cb){ // req-tail
+    var url = obj.url;
+    var full_scan = obj.full_scan;
+
     var options = {
         hostname: urlp(url).hostname,
         port: urlp(url).port,
@@ -55,12 +16,24 @@ function req_tail(http, url, cb){
             Range: range
         }
     };
+
+    var http;
+    var protocol = urlp(url).protocol;
+    if(protocol === 'https:'){
+        http = https_anc;
+    }else if(protocol === 'http:'){
+        http = http_anc;
+    }else{
+        cb('it seems that the url is not http nor https');
+    }
+    
     var req = http.request(options, function(res){
         if(res.statusCode !== 206){
-            cb('can\'t get duration in an easy way...');
             req.abort();
+            req_head(http, url, full_scan, cb);
             return;
         }
+        
         var count = 0;
         res.on('readable', function(){
             var chunk = res.read();
@@ -79,14 +52,53 @@ function req_tail(http, url, cb){
                     cb(err);
                 }
             }else{
-                if(count > 10240){
-                    cb('can\'t get duration in an easy way');
+                if(count >= 10240){
+                    req_head(http, url, full_scan, cb);
                     req.abort();
                     return;
                 }
             }
         });
     });
+
+    req.end();
+    req.on('error', function(e){
+        console.log('problem with request: '+e.message);
+    });
+}
+function req_head(http, url, full_scan, cb){
+    var req = http.request(url, function(res){
+        var count = 0;
+        res.on('readable', function(){
+            var chunk = res.read();
+            var offset;
+            if(chunk === null){
+                cb('no mp3 header found');
+                req.abort();
+                return;
+            }
+            count+=chunk.length;
+            if((offset = chunk.indexOf(new Buffer([0x6D, 0x76, 0x68, 0x64]))) !== -1){
+                req.abort();
+                try{
+                    var buf = new Buffer(chunk);
+                    var time_scale = buf.slice(offset+16,offset+20).readInt32BE();
+                    var duration = buf.slice(offset+20,offset+24).readInt32BE();
+                    var result = duration/time_scale;
+                    cb(null, result);
+                }catch(err){
+                    cb(err);
+                }
+            }else{
+                if(count >= 10240 && !full_scan){
+                    cb('can\'t get duration in an easy way(no mp3 header found)');
+                    req.abort();
+                    return;
+                }
+            }
+        });
+    });
+
     req.end();
     req.on('error', function(e){
         console.log('problem with request: '+e.message);
--- test/test.js
+++ test/test.js
@@ -3,16 +3,31 @@ var omd = require('./../index');
 describe('online_mp4_duration', function(){
     describe('http', function(){
         it('should return 13.504sec', function(done){
-            omd('http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_2mb.mp4', function(err,duration){
+            omd({
+                    url: 'http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_2mb.mp4',
+                    full_scan: false
+                }, function(err,duration){
                 assert.equal(!err, true);
                 assert.equal(duration, 13.504);
+                done();
+            });
+        }).timeout(100000);
+        it('should return no mp3 header found', function(done){
+            omd({
+                    url: 'http://stackoverflow.com/questions/1307929/javascript-dom-load-events-execution-sequence-and-document-ready',
+                    full_scan: true
+                }, function(err,duration){
+                assert.equal(err, 'no mp3 header found');
                 done();
             });
         }).timeout(100000);
     });
     describe('https', function(){
         it('should be 203.91763333333333sec', function(done){
-            omd('https://docs.google.com/uc?id=0B8NqhmxRdDloRl9JcWlFbWhpT1E', function(err, duration){
+            omd({
+                    url: 'https://docs.google.com/uc?id=0B8NqhmxRdDloRl9JcWlFbWhpT1E',
+                    full_scan: false
+                }, function(err, duration){
                 assert.equal(!err, true);
                 assert.equal(duration, 203.91763333333333);
                 done();
