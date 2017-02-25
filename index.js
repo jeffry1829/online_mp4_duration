cd0d059be1475100ab74b7ae0ebad8c2b754407d
petjelinux <jeffry1829@gmail.com>
2017-02-26 02:04:16 +0800

    fix tail request Buffer compare, full or head left

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var https_anc = require('follow-redirects').https;
 var urlp = require('url').parse;
 var Range = require('http-range').Range;
 var range = new Range('bytes', '-10240');
-function run(obj, cb){ // req-tail
+
+function run(obj, cb) { // req-tail
     var url = obj.url;
     var full_scan = obj.full_scan;
 
@@ -19,78 +20,85 @@ function run(obj, cb){ // req-tail
 
     var http;
     var protocol = urlp(url).protocol;
-    if(protocol === 'https:'){
+    if (protocol === 'https:') {
         http = https_anc;
-    }else if(protocol === 'http:'){
+    } else if (protocol === 'http:') {
         http = http_anc;
-    }else{
+    } else {
         cb('it seems that the url is not http nor https');
     }
-    
-    var req = http.request(options, function(res){
-        if(res.statusCode !== 206){
+
+    var req = http.request(options, function(res) {
+        if (res.statusCode !== 206) {
             req.abort();
             req_head(http, url, full_scan, cb);
             return;
         }
-        
+
+        var chunk;
         var count = 0;
-        res.on('readable', function(){
-            var chunk = res.read();
+        res.on('readable', function() {
+            var this_chunk = res.read();
             var offset;
-            if(chunk === null) return;
-            count+=chunk.length;
-            if((offset = chunk.indexOf(new Buffer([0x6D, 0x76, 0x68, 0x64]))) !== -1){
-                req.abort();
-                try{
-                    var buf = new Buffer(chunk);
-                    var time_scale = buf.slice(offset+16,offset+20).readInt32BE();
-                    var duration = buf.slice(offset+20,offset+24).readInt32BE();
-                    var result = duration/time_scale;
-                    cb(null, result);
-                }catch(err){
-                    cb(err);
-                }
-            }else{
-                if(count >= 10240){
+            if (this_chunk === null) {
+                if ((offset = chunk.indexOf(new Buffer([0x6D, 0x76, 0x68, 0x64]))) !== -1) {
+                    req.abort();
+                    try {
+                        var buf = new Buffer(chunk);
+                        var time_scale = buf.slice(offset + 16, offset + 20).readInt32BE();
+                        var duration = buf.slice(offset + 20, offset + 24).readInt32BE();
+                        var result = duration / time_scale;
+                        cb(null, result);
+                    } catch (err) {
+                        cb(err);
+                    }
+                } else {
                     req_head(http, url, full_scan, cb);
                     req.abort();
                     return;
                 }
+                return;
             }
+            if (!chunk)
+                chunk = this_chunk;
+            else
+                chunk = Buffer.concat([chunk, this_chunk], chunk.length + this_chunk.length);
+
+            count += this_chunk.length; // if null, the length is uncaculatable
         });
     });
 
     req.end();
-    req.on('error', function(e){
-        console.log('problem with request: '+e.message);
+    req.on('error', function(e) {
+        console.log('problem with request: ' + e.message);
     });
 }
-function req_head(http, url, full_scan, cb){
-    var req = http.request(url, function(res){
+
+function req_head(http, url, full_scan, cb) {
+    var req = http.request(url, function(res) {
         var count = 0;
-        res.on('readable', function(){
+        res.on('readable', function() {
             var chunk = res.read();
             var offset;
-            if(chunk === null){
+            if (chunk === null) {
                 cb('no mp4 header found');
                 req.abort();
                 return;
             }
-            count+=chunk.length;
-            if((offset = chunk.indexOf(new Buffer([0x6D, 0x76, 0x68, 0x64]))) !== -1){
+            count += chunk.length;
+            if ((offset = chunk.indexOf(new Buffer([0x6D, 0x76, 0x68, 0x64]))) !== -1) {
                 req.abort();
-                try{
+                try {
                     var buf = new Buffer(chunk);
-                    var time_scale = buf.slice(offset+16,offset+20).readInt32BE();
-                    var duration = buf.slice(offset+20,offset+24).readInt32BE();
-                    var result = duration/time_scale;
+                    var time_scale = buf.slice(offset + 16, offset + 20).readInt32BE();
+                    var duration = buf.slice(offset + 20, offset + 24).readInt32BE();
+                    var result = duration / time_scale;
                     cb(null, result);
-                }catch(err){
+                } catch (err) {
                     cb(err);
                 }
-            }else{
-                if(count >= 10240 && !full_scan){
+            } else {
+                if (count >= 10240 && !full_scan) {
                     cb('can\'t get duration in an easy way(no mp4 header found)');
                     req.abort();
                     return;
@@ -100,8 +108,8 @@ function req_head(http, url, full_scan, cb){
     });
 
     req.end();
-    req.on('error', function(e){
-        console.log('problem with request: '+e.message);
+    req.on('error', function(e) {
+        console.log('problem with request: ' + e.message);
     });
 }
 module.exports = run;
